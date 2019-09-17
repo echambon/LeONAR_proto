ca1793c61bffedeaef0a7c2defab81ef3b8d3495
Emmanuel Chambon <echambon.contact@free.fr>
2019-09-17 21:51:38 +0200

    corrections & refpixel update based on lat/lon

--- src/utils/tiles/LatLonToTileNumber.m
+++ src/utils/tiles/LatLonToTileNumber.m
@@ -2,17 +2,30 @@ function tile = LatLonToTileNumber(latitude,longitude,zoom)
 %LATLON2TILENUMBER Summary of this function goes here
 %   Detailed explanation goes here
 
-n           = 2^zoom;
+n               = 2^zoom;
 
 % Initializtion
-tile        = Tile;
+tile            = Tile;
 
 % X
-tile.Column = uint32(n * ((longitude + 180) / 360));
+tile.Column     = floor(n * ((longitude + 180) / 360));
 
 % Y
-lat_rad     = deg2rad(latitude);
-tile.Line   = uint32(n * (1 - (log(tan(lat_rad) + sec(lat_rad)) / pi)) /2);
+lat_rad         = deg2rad(latitude);
+tile.Line       = floor(n * (1 - (log(tan(lat_rad) + sec(lat_rad)) / pi)) /2);
+
+%% Also store pixel
+[tmp_latitude,tmp_longitude] = TileNumberToLatLon(tile,zoom);
+error_longitude = longitude - tmp_longitude; % > 0 to the est
+error_latitude  = latitude - tmp_latitude; % < 0 to the south
+
+x = error_longitude * n / 360;
+
+lat_rad_error = atan(sinh(pi * (1 - 2*tile.Line / n)))-atan(sinh(pi * (1 - 2 * (tile.Line-1)/n))); % negative toward south
+y = error_latitude/rad2deg(lat_rad_error);
+
+% Update reference pixel
+tile.RefPixel   = floor([256*x+1 256*y+1]);
 
 end
 
