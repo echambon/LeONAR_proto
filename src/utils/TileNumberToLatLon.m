f5526db58b498d72a1420eca832bae61fb644e90
Emmanuel Chambon <echambon.contact@free.fr>
2019-09-17 19:04:32 +0200

    conversion tile number to lat/lon

---
+++ src/utils/TileNumberToLatLon.m
@@ -0,0 +1,13 @@
+function [latitude,longitude] = TileNumberToLatLon(tile,zoom)
+%TILENUMBERTOLATLON Summary of this function goes here
+%   latitude:  in degrees
+%   longitude: in degrees
+
+n           = 2^zoom;
+longitude   = tile.Column / n * 360 - 180;
+
+lat_rad     = atan(sinh(pi * (1 - 2*tile.Line / n)));
+latitude    = rad2deg(lat_rad);
+
+end
+
