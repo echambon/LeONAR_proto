2ddc9163779111d0cb06110f6b2e5dd260f5c583
Emmanuel Chambon <echambon.contact@free.fr>
2019-09-18 18:59:36 +0200

    automated min/max zoom determination

---
+++ src/utils/tiles/GetMinMaxZoomFromTilesFolder.m
@@ -0,0 +1,32 @@
+function [minZoom,maxZoom] = GetMinMaxZoomFromTilesFolder(tiles_folder)
+%GETMINMAXZOOM Summary of this function goes here
+%   Detailed explanation goes here
+
+% Checking last character is \ or /
+t_tiles_folder = tiles_folder;
+if ~strcmp(t_tiles_folder,filesep)
+    t_tiles_folder = [t_tiles_folder filesep];
+end
+
+% Listing files and folders in tiles_folder
+files = dir(t_tiles_folder);
+
+% Identifying folders
+dirFlags = [files.isdir];
+
+% Identifying real folders i.e. not '.' or '..'
+realDirFlags = cellfun(@(x)~(strcmp(x,'.') || strcmp(x,'..')),{files.name});
+
+% Extracting subfolders
+subFolders = files(dirFlags & realDirFlags);
+
+% Subfolders names and conversion to numeric type
+subFoldersNames = {subFolders.name};
+zoomLevels = cellfun(@(x)str2double(x),subFoldersNames);
+
+% Deduction of min and max zoom levels
+minZoom = min(zoomLevels);
+maxZoom = max(zoomLevels);
+
+end
+
