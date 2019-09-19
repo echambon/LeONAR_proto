0daba40f6b1ac649fb4c4f3264055354bcef8b1e
Emmanuel Chambon <echambon.contact@free.fr>
2019-09-19 18:57:21 +0200

    moving code from appdesigner to callback

--- src/gui/callbacks/leonar_WindowScrollWheel.m
+++ src/gui/callbacks/leonar_WindowScrollWheel.m
@@ -1,9 +1,15 @@
-function wheel_count = leonar_WindowScrollWheel(~,event)
+function wheel_count = leonar_WindowScrollWheel(app,event)
 %LEONAR_WINDOWSROLLWHEEL Summary of this function goes here
 %   Detailed explanation goes here
 
 % Extracting wheel count (-1 for wheel down, +1 for wheel up)
 wheel_count = event.VerticalScrollCount;
+
+% Updating map current zoom and safeguarding against unavailable zooms
+app.mapCurrentZoom = max(min(app.mapCurrentZoom - app.cbMouseWheelCount,app.mapMaxZoom),app.mapMinZoom);
+
+%% Debug
+app.DebugLabel.Text = num2str(app.mapCurrentZoom);
 
 end
 
