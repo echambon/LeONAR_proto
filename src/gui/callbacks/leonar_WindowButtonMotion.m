924df263d477fff3ea53acd63b034c6672eb6a3d
Emmanuel Chambon <echambon.contact@free.fr>
2019-09-19 20:44:48 +0200

    first motion counter implementation

--- src/gui/callbacks/leonar_WindowButtonMotion.m
+++ src/gui/callbacks/leonar_WindowButtonMotion.m
@@ -3,8 +3,17 @@ function leonar_WindowButtonMotion(app,event)
 %   Detailed explanation goes here
 
 if app.mapClicked
-    app.mapMotionCounter = app.mapMotionCounter + 1;
-    disp(app.mapMotionCounter);
+    tmpPreviousIntersectionPoint   = app.mapFigureIntersectionPoint;
+    
+    % Updating motion counter
+    app.mapMotionCounter(1) = app.mapMotionCounter(1) + sign(event.IntersectionPoint(1) - tmpPreviousIntersectionPoint(1));
+    app.mapMotionCounter(2) = app.mapMotionCounter(2) + sign(event.IntersectionPoint(2) - tmpPreviousIntersectionPoint(2));
+    
+    % Debug
+    app.DebugLabel.Text = [num2str(app.mapMotionCounter(1)) ' - ' num2str(app.mapMotionCounter(2))];
+    
+    % Update previous intersection point
+    app.mapFigureIntersectionPoint = event.IntersectionPoint;
 end
 
 end
