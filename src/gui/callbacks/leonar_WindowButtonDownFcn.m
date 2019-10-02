ed51fb13dbf19dbe92b9edb7d46c0142393c732e
Emmanuel Chambon <echambon.contact@free.fr>
2019-10-02 22:16:43 +0200

    cosmetics

--- src/gui/callbacks/leonar_WindowButtonDownFcn.m
+++ src/gui/callbacks/leonar_WindowButtonDownFcn.m
@@ -6,13 +6,13 @@ function [source,coords_source,mouse_source] = leonar_WindowButtonDownFcn(app,ev
 source = '';
 
 % Current point
-current_point = event.Source.CurrentPoint;
+current_point = event.Source.CurrentPoint
 
 % Checking if we clicked inside the map object
 % Also returns coordinates in the considered object
 [is_inside_map,coords_source] = gui_isInsidePosition(current_point,app.Map);
 if is_inside_map
-    source = 'Map'
+    source = 'Map';
 end
 
 % Extracting mouse source
@@ -23,4 +23,3 @@ end
 mouse_source = event.Source.SelectionType;
 
 end
-
