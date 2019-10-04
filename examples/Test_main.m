2dd2151fc1b72e6ab75d276e94fccba498fd6453
Emmanuel Chambon <echambon.contact@free.fr>
2019-10-04 18:35:35 +0200

    example update (added dependency)

--- examples/Test_main.m
+++ examples/Test_main.m
@@ -3,5 +3,8 @@ clear; close all;
 % Adding LeONAR src to the path
 addpath('D:\GitHub\LeONAR_proto\src');
 
+% Adding dependencies to path
+addpath('D:\GitHub\mlapptools');
+
 % Executing LeONAR
 main();
